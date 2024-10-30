df6c193f7d904e9e224a1c62b14a4c24c304f50d
Frank Femia <femiaf13@gmail.com>
2024-10-29 20:29:05 -0400

    More cleanup

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,47 +1,15 @@
-use tauri::Manager;
-
-use diesel::prelude::*;
-use diesel_migrations::{embed_migrations, EmbeddedMigrations, MigrationHarness};
-use models::Food;
-use std::env;
-
 pub mod database;
 pub mod models;
 pub mod schema;
+
+use tauri::Manager;
+use diesel_migrations::{embed_migrations, EmbeddedMigrations, MigrationHarness};
+use database::*;
 
 const MIGRATIONS: EmbeddedMigrations = embed_migrations!();
 
 struct AppState {
     database_url: String,
-}
-
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
-fn greet(app_handle: tauri::AppHandle, food_id: &str) -> Food {
-    let database_url = app_handle.state::<AppState>().database_url.clone();
-
-    /**
-     *  let pattern = format!("%{}%", target);
-     *
-     *    let connection = &mut establish_connection();
-     *    let num_deleted = diesel::delete(posts.filter(title.like(pattern)))
-     *    .execute(connection)
-     *    .expect("Error deleting posts");
-     */
-    // println!("{}", database_url);
-    use self::schema::foods::dsl::*;
-
-    let connection = &mut database::establish_connection(database_url);
-
-    let food_id_int: i32 = food_id.parse().unwrap_or(1);
-    let result = foods
-        .filter(id.eq(food_id_int))
-        // Haven't tried without the select but I think it will automatically do *
-        // .select(Food::as_select())
-        .load::<Food>(connection)
-        .expect("Error loading food");
-
-    result[0].clone()
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
@@ -56,7 +24,7 @@ pub fn run() {
             let binding = path.join("food.db");
             let database_url = binding.to_str().unwrap().to_string();
 
-            let connection = &mut database::establish_connection(database_url.clone());
+            let connection = &mut establish_connection(database_url.clone());
             let _ = connection.run_pending_migrations(MIGRATIONS);
 
             app.manage(AppState {
@@ -67,9 +35,8 @@ pub fn run() {
         })
         .plugin(tauri_plugin_shell::init())
         .invoke_handler(tauri::generate_handler![
-            greet,
-            database::find_food_by_id,
-            database::find_food_by_description
+            find_food_by_id,
+            find_food_by_description
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
