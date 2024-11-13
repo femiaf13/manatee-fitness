29321d3edd40602daf519509206b2efff7a03bdd
Frank Femia <femiaf13@gmail.com>
2024-11-12 20:13:01 -0500

    Default date for meal dto to be selected day in app

--- src/app/models/meal.model.ts
+++ src/app/models/meal.model.ts
@@ -1,11 +1,16 @@
+import { inject } from '@angular/core';
 import { DateService } from '@services/date.service';
 import { format } from 'date-fns';
 
 export class MealDTO {
+    dateService = inject(DateService);
     meal_date: string;
     meal_name: string;
 
-    constructor(date: Date = new Date(), name: string = 'New Meal') {
+    constructor(date: Date | null = null, name: string = 'New Meal') {
+        if (date === null) {
+            date = this.dateService.selectedDate();
+        }
         this.meal_date = format(date, DateService.DATE_FORMAT_STRING);
         this.meal_name = name;
     }
