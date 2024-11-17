11b057d1b8f2f42b8270dfbba86913f5f584d27b
Frank Femia <femiaf13@gmail.com>
2024-11-16 19:05:39 -0500

    Update food model with changes

--- src/app/models/food.model.ts
+++ src/app/models/food.model.ts
@@ -1,30 +1,27 @@
-export class Food {
-    id!: number;
-    description!: string;
-    calories_per_100g!: number;
-    grams_per_serving!: number;
-    serving_text!: string;
-    calories_per_serving!: number;
-    fat!: number;
-    carbs!: number;
-    protein!: number;
-    cholesterol!: number;
-    fiber!: number;
-}
-
 export class FoodDTO {
+    barcode: string;
     description: string;
+    brand: string;
     calories_per_100g: number;
     grams_per_serving: number;
     serving_text: string;
+    // Fat in g per 100g
     fat: number;
+    // Carbs in g per 100g
     carbs: number;
+    // Protein in g per 100g
     protein: number;
+    // Cholesterol in mg per 100g
     cholesterol: number;
+    // Fiber in _ per 100g
     fiber: number;
+    // Sodium in mg per 100g
+    sodium: number;
 
     constructor() {
+        this.barcode = '';
         this.description = '';
+        this.brand = '';
         this.calories_per_100g = 0;
         this.grams_per_serving = 0;
         this.serving_text = '';
@@ -33,7 +30,12 @@ export class FoodDTO {
         this.protein = 0;
         this.cholesterol = 0;
         this.fiber = 0;
+        this.sodium = 0;
     }
+}
+
+export class Food extends FoodDTO {
+    id!: number;
 }
 
 export class SummedFood {
