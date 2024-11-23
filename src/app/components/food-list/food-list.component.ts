1379d229ca97fd44de33771c5f3e9029612f58df
Frank Femia <femiaf13@gmail.com>
2024-11-23 12:49:56 -0500

    Leave the food list alone for now

--- src/app/components/food-list/food-list.component.ts
+++ src/app/components/food-list/food-list.component.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component, input } from '@angular/core';
 import { MatListModule } from '@angular/material/list';
 import { Food } from '@models/food.model';
@@ -5,7 +6,7 @@ import { Food } from '@models/food.model';
 @Component({
     selector: 'app-food-list',
     standalone: true,
-    imports: [MatListModule],
+    imports: [CommonModule, MatListModule],
     templateUrl: './food-list.component.html',
     styleUrl: './food-list.component.css',
 })
