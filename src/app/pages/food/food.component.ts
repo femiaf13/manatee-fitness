a204afb5c6e6e5a5007d5e5d694b062829a166e6
Frank Femia <femiaf13@gmail.com>
2024-11-23 12:54:50 -0500

    Use material autocomplete instead for searching foods to add

--- src/app/pages/food/food.component.ts
+++ src/app/pages/food/food.component.ts
@@ -1,27 +1,29 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { CommonModule } from '@angular/common';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs';
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
+
 import { Food } from '@models/food.model';
 import { DatabaseService } from '@services/database.service';
-
-import { FoodListComponent } from '@components/food-list/food-list.component';
 
 @Component({
     selector: 'app-page-food',
     standalone: true,
     imports: [
         CommonModule,
+        MatAutocompleteModule,
+        MatDividerModule,
         MatButtonModule,
         MatFormFieldModule,
         MatInputModule,
         ReactiveFormsModule,
-        FoodListComponent,
     ],
     templateUrl: './food.component.html',
     styleUrl: './food.component.css',
