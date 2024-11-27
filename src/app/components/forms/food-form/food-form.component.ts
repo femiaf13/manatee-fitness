79f44cfce1d581009d7571f1cbcff3ea1ef41d2e
Frank Femia <femiaf13@gmail.com>
2024-11-27 16:34:20 -0500

    food form handles nutrients as per serving now

--- src/app/components/forms/food-form/food-form.component.ts
+++ src/app/components/forms/food-form/food-form.component.ts
@@ -34,30 +34,40 @@ export class FoodFormComponent {
         gramsPerServing: [0, Validators.required],
         caloriesPerServing: [0, Validators.required],
         servingText: [''],
-        fat: [0],
-        carbs: [0],
-        protein: [0],
-        cholesterol: [0],
-        fiber: [0],
-        sodium: [0],
+        fatPerServing: [0],
+        carbsPerServing: [0],
+        proteinPerServing: [0],
+        cholesterolPerServing: [0],
+        fiberPerServing: [0],
+        sodiumPerServing: [0],
     });
+
+    gramsPerServing$ = this.foodForm.controls.gramsPerServing.valueChanges;
 
     onSubmit() {
         const rawFormValues = this.foodForm.getRawValue();
+        // Convert the per servings numbers back to per 100g for the DB
         const caloriesPer100g = (rawFormValues.caloriesPerServing / rawFormValues.gramsPerServing) * 100;
+        const fatPer100g = (rawFormValues.fatPerServing / rawFormValues.gramsPerServing) * 100;
+        const carbsPer100g = (rawFormValues.carbsPerServing / rawFormValues.gramsPerServing) * 100;
+        const proteinPer100g = (rawFormValues.proteinPerServing / rawFormValues.gramsPerServing) * 100;
+        const cholesterolPer100g = (rawFormValues.cholesterolPerServing / rawFormValues.gramsPerServing) * 100;
+        const fiberPer100g = (rawFormValues.fiberPerServing / rawFormValues.gramsPerServing) * 100;
+        const sodiumPer100g = (rawFormValues.sodiumPerServing / rawFormValues.gramsPerServing) * 100;
+
         const foodDtoOutput: FoodDTO = new FoodDTO(
             rawFormValues.barcode,
             rawFormValues.description,
             rawFormValues.brand,
-            +caloriesPer100g.toFixed(2),
+            +caloriesPer100g.toFixed(1),
             rawFormValues.gramsPerServing,
             rawFormValues.servingText,
-            rawFormValues.fat,
-            rawFormValues.carbs,
-            rawFormValues.protein,
-            rawFormValues.cholesterol,
-            rawFormValues.fiber,
-            rawFormValues.sodium
+            +fatPer100g.toFixed(1),
+            +carbsPer100g.toFixed(1),
+            +proteinPer100g.toFixed(1),
+            +cholesterolPer100g.toFixed(1),
+            +fiberPer100g.toFixed(1),
+            +sodiumPer100g.toFixed(1)
         );
         this.outputFood.emit(foodDtoOutput);
     }
@@ -70,20 +80,38 @@ export class FoodFormComponent {
         effect(() => {
             const inputFood = this.inputFood();
 
+            // Show the nutrients as per serving numbers not as the per 100g numbers from the db
             untracked(() => {
-                const caloriesServing = (inputFood.grams_per_serving / 100) * inputFood.calories_per_100g;
                 this.foodForm.controls.barcode.setValue(inputFood.barcode);
                 this.foodForm.controls.description.setValue(inputFood.description);
                 this.foodForm.controls.brand.setValue(inputFood.brand);
-                this.foodForm.controls.caloriesPerServing.setValue(+caloriesServing.toFixed(2));
-                this.foodForm.controls.gramsPerServing.setValue(inputFood.grams_per_serving);
+
+                // Handle foods with 0 grams per serving. Auto set serving to 100grams
+                const gramsPerServing = inputFood.grams_per_serving > 0 ? inputFood.grams_per_serving : 100;
+                // Serving size in 100g, so 133 would be 1.33
+                const servingMultiplier = gramsPerServing / 100;
+                const caloriesServing = servingMultiplier * inputFood.calories_per_100g;
+                this.foodForm.controls.caloriesPerServing.setValue(+caloriesServing.toFixed(1));
+                this.foodForm.controls.gramsPerServing.setValue(gramsPerServing);
                 this.foodForm.controls.servingText.setValue(inputFood.serving_text);
-                this.foodForm.controls.fat.setValue(inputFood.fat);
-                this.foodForm.controls.carbs.setValue(inputFood.carbs);
-                this.foodForm.controls.protein.setValue(inputFood.protein);
-                this.foodForm.controls.cholesterol.setValue(inputFood.cholesterol);
-                this.foodForm.controls.fiber.setValue(inputFood.fiber);
-                this.foodForm.controls.sodium.setValue(inputFood.sodium);
+
+                const fatServing = servingMultiplier * inputFood.fat;
+                this.foodForm.controls.fatPerServing.setValue(+fatServing.toFixed(1));
+
+                const carbsServing = servingMultiplier * inputFood.carbs;
+                this.foodForm.controls.carbsPerServing.setValue(+carbsServing.toFixed(1));
+
+                const proteinServing = servingMultiplier * inputFood.protein;
+                this.foodForm.controls.proteinPerServing.setValue(+proteinServing.toFixed(1));
+
+                const cholesterolServing = servingMultiplier * inputFood.cholesterol;
+                this.foodForm.controls.cholesterolPerServing.setValue(+cholesterolServing.toFixed(1));
+
+                const fiberServing = servingMultiplier * inputFood.fiber;
+                this.foodForm.controls.fiberPerServing.setValue(+fiberServing.toFixed(1));
+
+                const sodiumServing = servingMultiplier * inputFood.sodium;
+                this.foodForm.controls.sodiumPerServing.setValue(+sodiumServing.toFixed(1));
             });
         });
     }
