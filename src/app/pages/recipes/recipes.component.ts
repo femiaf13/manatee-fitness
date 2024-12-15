1fbe089a3470d950150fc26e74d7d26051652381
Frank Femia <femiaf13@gmail.com>
2024-12-14 21:57:17 -0500

    start recipe expansion panel

--- src/app/pages/recipes/recipes.component.ts
+++ src/app/pages/recipes/recipes.component.ts
@@ -1,5 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { Component, effect, inject, OnInit, signal, untracked } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 import { Recipe } from '@models/recipe.model';
 import { RecipeFood } from '@models/recipefood.model';
 import { DatabaseService } from '@services/database.service';
@@ -13,7 +17,7 @@ interface RecipeWithRecipeFoods {
 @Component({
     selector: 'app-page-recipes',
     standalone: true,
-    imports: [CommonModule],
+    imports: [CommonModule, MatButtonModule, MatExpansionModule, MatListModule, MatIconModule],
     templateUrl: './recipes.component.html',
     styleUrl: './recipes.component.css',
 })
