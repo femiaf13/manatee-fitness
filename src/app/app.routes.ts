5083a5ac9d2e1691557589f8c9e21459e7b70a4f
Frank Femia <femiaf13@gmail.com>
2024-12-23 15:09:01 -0500

    Add blank stats page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { DiaryComponent } from '@pages/diary/diary.component';
 import { FoodPageComponent } from '@pages/food/food.component';
 import { GoalsComponent } from '@pages/goals/goals.component';
 import { RecipesPageComponent } from '@pages/recipes/recipes.component';
+import { StatsPageComponent } from '@pages/stats/stats.component';
 
 export const routes: Routes = [
     { path: 'diary', component: DiaryComponent },
     { path: 'goals', component: GoalsComponent },
     { path: 'food', component: FoodPageComponent },
     { path: 'recipes', component: RecipesPageComponent },
+    { path: 'stats', component: StatsPageComponent },
     { path: '**', component: DiaryComponent },
 ];
