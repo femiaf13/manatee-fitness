eba1eeb4d9c42dbf8f9571b8ede8e3d6b539a984
Frank Femia <femiaf13@gmail.com>
2025-01-13 18:56:56 -0500

    beginning of calorie calculation form setup

--- src/app/pages/goals/goals.component.ts
+++ src/app/pages/goals/goals.component.ts
@@ -1,22 +1,37 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, effect, inject, OnInit, signal, untracked } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
 import { NonNullableFormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { Goal } from '@models/goal.model';
 import { DatabaseService } from '@services/database.service';
 import { DateService } from '@services/date.service';
+import { UnitConversionService } from '@services/unit-conversion.service';
 
 @Component({
     selector: 'app-page-goals',
-    imports: [ReactiveFormsModule, MatButtonModule, MatFormFieldModule, MatGridListModule, MatInputModule],
+    imports: [
+        CommonModule,
+        ReactiveFormsModule,
+        MatButtonModule,
+        MatFormFieldModule,
+        MatGridListModule,
+        MatInputModule,
+        MatSelectModule,
+        MatSlideToggleModule,
+    ],
     templateUrl: './goals.component.html',
     styleUrl: './goals.component.css',
 })
 export class GoalsComponent implements OnInit {
     dateService = inject(DateService);
     databaseService = inject(DatabaseService);
+    unitConversionService = inject(UnitConversionService);
 
     private formBuilder = inject(NonNullableFormBuilder);
     goalForm = this.formBuilder.group({
@@ -28,6 +43,22 @@ export class GoalsComponent implements OnInit {
         fiber: [0, Validators.min(0)],
         sodium: [0, Validators.min(0)],
     });
+
+    nutritionForm = this.formBuilder.group({
+        weight: [0, [Validators.required, Validators.min(0)]],
+        height: [0, [Validators.required, Validators.min(0)]],
+        age: [0, [Validators.required, Validators.min(18)]],
+        activityLevel: [1.0, [Validators.required, Validators.min(1.0)]],
+        useMetric: [false, [Validators.required]],
+    });
+
+    useMetricSignal = toSignal(this.nutritionForm.controls.useMetric.valueChanges, {
+        initialValue: this.nutritionForm.controls.useMetric.value,
+    });
+
+    weightUnit = signal<string>('lb');
+    heightUnit = signal<string>('in');
+    useMetric = signal<boolean>(false);
 
     async onSubmit() {
         const goal = new Goal(
@@ -57,5 +88,28 @@ export class GoalsComponent implements OnInit {
 
     constructor() {
         this.dateService.setTitle('Goals');
+
+        effect(() => {
+            const useMetric = this.useMetricSignal();
+
+            untracked(() => {
+                const weightUnit = useMetric ? 'kg' : 'lb';
+                const convertedWeight = this.unitConversionService
+                    .convert(this.nutritionForm.controls.weight.value)
+                    .from(this.weightUnit())
+                    .to(weightUnit);
+                this.weightUnit.set(weightUnit);
+
+                const heightUnit = useMetric ? 'cm' : 'in';
+                const convertedHeight = this.unitConversionService
+                    .convert(this.nutritionForm.controls.height.value)
+                    .from(this.heightUnit())
+                    .to(heightUnit);
+                this.heightUnit.set(heightUnit);
+
+                this.nutritionForm.controls.weight.setValue(+convertedWeight.toFixed(1));
+                this.nutritionForm.controls.height.setValue(+convertedHeight.toFixed(1));
+            });
+        });
     }
 }
