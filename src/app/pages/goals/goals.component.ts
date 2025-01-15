9d0fc82f8ae6532fa4ce9b790d66ebbdda257cdf
Frank Femia <femiaf13@gmail.com>
2025-01-14 21:27:15 -0500

    Add macro calculation based on goal calories

--- src/app/pages/goals/goals.component.ts
+++ src/app/pages/goals/goals.component.ts
@@ -37,8 +37,6 @@ export class GoalsComponent implements OnInit {
     snackBar = inject(MatSnackBar);
     unitConversionService = inject(UnitConversionService);
 
-    readonly DANGEROUS_CALORIE_THRESHOLD = 1200;
-
     private formBuilder = inject(NonNullableFormBuilder);
     goalForm = this.formBuilder.group({
         calories: [0, [Validators.required, Validators.min(0)]],
@@ -50,11 +48,20 @@ export class GoalsComponent implements OnInit {
         sodium: [0, Validators.min(0)],
     });
 
+    caloriesSignal = toSignal(this.goalForm.controls.calories.valueChanges, {
+        initialValue: this.goalForm.controls.calories.value,
+    });
+
+    /**
+     * Calorie Stuff
+     */
+    readonly DANGEROUS_CALORIE_THRESHOLD = 1200;
+
     weightUnit = signal<string>('lb');
     heightUnit = signal<string>('in');
 
     nutritionForm = this.formBuilder.group({
-        weight: [0, [Validators.required, Validators.min(50)]],
+        weight: [0, [Validators.required, Validators.min(25)]],
         height: [0, [Validators.required, Validators.min(10)]],
         age: [0, [Validators.required, Validators.min(18)]],
         activityLevel: [1.0, [Validators.required, Validators.min(1.0)]],
@@ -99,12 +106,63 @@ export class GoalsComponent implements OnInit {
     });
 
     /**
+     * Macronutrient Stuff
+     */
+    readonly MINIMUM_FAT_PERCENTAGE = 20;
+    readonly MINIMUM_CARBS_PERCENTAGE = 40;
+    readonly MINIMUM_PROTEIN_PERCENTAGE = 10;
+    // Defaults are sourced by being in the middle of generally
+    // recommended ranges
+    readonly DEFAULT_FAT_PERCENTAGE = 30;
+    readonly DEFAULT_CARBS_PERCENTAGE = 50;
+    readonly DEFAULT_PROTEIN_PERCENTAGE = 20;
+    readonly CALORIES_PER_GRAM_FAT = 9;
+    readonly CALORIES_PER_GRAM_CARBS = 4;
+    readonly CALORIES_PER_GRAM_PROTEIN = 4;
+
+    macrosForm = this.formBuilder.group({
+        fatPercentage: [0, [Validators.required, Validators.min(this.MINIMUM_FAT_PERCENTAGE)]],
+        carbsPercentage: [0, [Validators.required, Validators.min(this.MINIMUM_CARBS_PERCENTAGE)]],
+        proteinPercentage: [0, [Validators.required, Validators.min(this.MINIMUM_PROTEIN_PERCENTAGE)]],
+    });
+
+    fatPercentageSignal = toSignal(this.macrosForm.controls.fatPercentage.valueChanges, {
+        initialValue: this.macrosForm.controls.fatPercentage.value,
+    });
+    fatGrams = computed(() => {
+        const fatPercentage = this.fatPercentageSignal() / 100;
+        const calories = this.caloriesSignal();
+        const fatGrams = (calories * fatPercentage) / this.CALORIES_PER_GRAM_FAT;
+        return +fatGrams.toFixed(0);
+    });
+
+    carbsPercentageSignal = toSignal(this.macrosForm.controls.carbsPercentage.valueChanges, {
+        initialValue: this.macrosForm.controls.carbsPercentage.value,
+    });
+    carbsGrams = computed(() => {
+        const carbsPercentage = this.carbsPercentageSignal() / 100;
+        const calories = this.caloriesSignal();
+        const carbGrams = (calories * carbsPercentage) / this.CALORIES_PER_GRAM_CARBS;
+        return +carbGrams.toFixed(0);
+    });
+
+    proteinPercentageSignal = toSignal(this.macrosForm.controls.proteinPercentage.valueChanges, {
+        initialValue: this.macrosForm.controls.proteinPercentage.value,
+    });
+    proteinGrams = computed(() => {
+        const proteinPercentage = this.proteinPercentageSignal() / 100;
+        const calories = this.caloriesSignal();
+        const proteinGrams = (calories * proteinPercentage) / this.CALORIES_PER_GRAM_PROTEIN;
+        return +proteinGrams.toFixed(0);
+    });
+
+    /**
      *
      * @param message Notification message
      * @param action Text used for the dismissal button
-     * @param duration Duration in ms before notification dismisses itself. Defaults to 15s
+     * @param duration Duration in ms before notification dismisses itself. Defaults to 5s
      */
-    private openSnackBar(message: string, action: string = 'Dismiss', duration = 15000) {
+    private openSnackBar(message: string, action: string = 'Dismiss', duration = 5000) {
         this.snackBar.open(message, action, {
             duration: duration,
         });
@@ -140,6 +198,26 @@ export class GoalsComponent implements OnInit {
         this.openSnackBar('Calories set!');
     }
 
+    async onSubmitMacros() {
+        const fatGrams = this.fatGrams();
+        const carbsGrams = this.carbsGrams();
+        const proteinGrams = this.proteinGrams();
+        const goal = new Goal(
+            this.goalForm.controls.calories.value,
+            fatGrams,
+            carbsGrams,
+            proteinGrams,
+            this.goalForm.controls.cholesterol.value,
+            this.goalForm.controls.fiber.value,
+            this.goalForm.controls.sodium.value
+        );
+        await this.databaseService.setGoal(goal);
+        this.goalForm.controls.fat.setValue(fatGrams);
+        this.goalForm.controls.carbs.setValue(carbsGrams);
+        this.goalForm.controls.protein.setValue(proteinGrams);
+        this.openSnackBar('Macros set!');
+    }
+
     async ngOnInit() {
         const goal = await this.databaseService.getGoal();
         if (goal !== undefined) {
@@ -150,6 +228,11 @@ export class GoalsComponent implements OnInit {
             this.goalForm.controls.cholesterol.setValue(goal.cholesterol);
             this.goalForm.controls.fiber.setValue(goal.fiber);
             this.goalForm.controls.sodium.setValue(goal.sodium);
+            // I need to do this to trigger the computed signals' initial computation
+            // for some reason if I init the form with these values instead it won't get called
+            this.macrosForm.controls.fatPercentage.setValue(30);
+            this.macrosForm.controls.carbsPercentage.setValue(50);
+            this.macrosForm.controls.proteinPercentage.setValue(20);
         }
     }
 
